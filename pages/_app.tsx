4f3ce3cc6cbf00bbaccba8e3fb923545846d4a34
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2022-01-03 15:29:59 +0200

    update tsconfig: add baseUrl & paths, update imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,7 @@
 import { AppProps } from 'next/app';
 
 import { Global } from '@emotion/react';
-
-import { globalStyles } from '../shared/globals';
+import { globalStyles } from '@shared/globals';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
