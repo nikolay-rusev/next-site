d7d9bf92af2c0e66b4cb1196123175508c27fc2f
Nikolay Rusev <nikolay.z.rusev@gmail.com>
2022-01-03 15:25:39 +0200

    update styles

--- shared/globals.ts
+++ shared/globals.ts
@@ -1,16 +1,29 @@
 import { css } from '@emotion/react';
 
-const globalStyles = css`
-  padding: 0;
-  margin: 0;
-  font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-  box-sizing: border-box;
-`;
+export const globalStyles = css`
+  :root {
+    --page--background: #222;
+    --page-text: #ddd;
+  }
 
-const linkStyles = css`
-  color: inherit;
-  text-decoration: none;
-`;
+  html,
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    background-color: var(--page-background);
+    color: var(--page-text);
+  }
 
-export { globalStyles, linkStyles };
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  *,
+  *::after,
+  *::before {
+    box-sizing: border-box;
+  }
+`;
